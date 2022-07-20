341bdd439b281ea5e7820ce473d25c7a68b1a462
ArashSp <a.sepahy25@gmail.com>
2022-07-20 17:40:00 +0430

    added the Script beta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,9 @@ import { defineNuxtConfig } from "nuxt";
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   css: ["~/assets/scss/main.scss"],
+  script: [
+    {
+      src: "./pages/rticle.js",
+    },
+  ],
 });
