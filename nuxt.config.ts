20939f6d6afaf433216d77965242345253faa627
mohsen <91586967+sepidsiah@users.noreply.github.com>
2022-11-09 17:06:52 +0330

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,15 +5,27 @@ import { defineNuxtConfig } from "nuxt";
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   css: ["@/assets/scss/main.scss"],
-  vite: {
-        css: {
-            preprocessorOptions: {
-                scss: {
-                    sourceMap: true,
-                    additionalData: `@import "@/assets/scss/main.scss";`,
-                },
-            },
-        }, 
+    vite : { 
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './')
+      }
     },
-  },
+    ssr: false,
+    css : { 
+        preprocessorOptions : { 
+            scss : { 
+                sourceMap : false , 
+                // additionalData : `@import "@/assets/mixin.scss";`,
+                additionalData (source, fp) {
+                  // All scss files ending with imports.scss
+                  // will not re-import additionalData
+                  if (fp.endsWith('variables.scss')) return source;
+                  // Use additionalData from legacy nuxt scss options
+                  return `@import "@/assets/scss/main.scss"; ${source}`
+                } 
+            } , 
+        } , 
+    } ,  
+ } ,
 );
