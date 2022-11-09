bd7c2435239af9e7c25ba44e494dbe01900a3cec
mohsen <91586967+sepidsiah@users.noreply.github.com>
2022-11-09 16:54:22 +0330

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,5 +5,15 @@ import { defineNuxtConfig } from "nuxt";
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   css: ["~/assets/scss/main.scss"],
+  vite: {
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    sourceMap: true,
+                    additionalData: `@import "~/assets/scss/main.scss";`,
+                },
+            },
+        }, 
+    },
   },
 );
