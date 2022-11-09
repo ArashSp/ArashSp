7d58aabf8f04569efe7bb094cb0d1454b4b53d30
mohsen <91586967+sepidsiah@users.noreply.github.com>
2022-11-09 17:16:09 +0330

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,5 @@
 import { defineNuxtConfig } from "nuxt";
-import { path } from "path";
-
+import path from 'path';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
