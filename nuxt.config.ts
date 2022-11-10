e5b228c5201c60836f09c72e1b1e13f5ea520887
ArashSp <a.sepahy25@gmail.com>
2022-11-10 22:24:22 +0330

    update nuxt.vonfig.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,30 +1,31 @@
 import { defineNuxtConfig } from "nuxt";
-import path from 'path';
+import path from "path";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   // css: ["@/assets/scss/main.scss"],
-    vite : { 
+  vite: {
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, './')
-      }
+        "@": path.resolve(__dirname, "./"),
+      },
     },
-   
-    css : { 
-        preprocessorOptions : { 
-            scss : { 
-                sourceMap : false , 
-                additionalData : `@import "@/assets/scss/main.scss";`,
-//                 additionalData (source, fp) {
-//                   // All scss files ending with imports.scss
-//                   // will not re-import additionalData
-//                   if (fp.endsWith('variables.scss')) return source;
-//                   // Use additionalData from legacy nuxt scss options
-//                   return `@import "@/assets/scss/main.scss"; ${source}`
-//                 } 
-            } , 
-        } , 
-    } ,  
-    },
+
+    css: ["~/assets/scss/main.scss"],
+    // css: {
+    //   //         preprocessorOptions : {
+    //   //             scss : {
+    //   //                 sourceMap : false ,
+    //   //                 additionalData : `@import "@/assets/scss/main.scss";`,
+    //   // //                 additionalData (source, fp) {
+    //   // //                   // All scss files ending with imports.scss
+    //   // //                   // will not re-import additionalData
+    //   // //                   if (fp.endsWith('variables.scss')) return source;
+    //   // //                   // Use additionalData from legacy nuxt scss options
+    //   // //                   return `@import "@/assets/scss/main.scss"; ${source}`
+    //   // //                 }
+    //   //             } ,
+    //   //         } ,
+    // },
+  },
 });
