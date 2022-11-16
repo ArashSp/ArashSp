eb6ffb746cad2953f9ca0084b5a5634e16adea7b
rezafrad <rezarad@gmail.com>
2022-11-16 13:38:49 +0330

    remove dist

--- dist/_nuxt/Group263.303f05a5.mjs
+++
@@ -1,1 +0,0 @@
-const e=""+new URL("Group427.1738efee.svg",import.meta.url).href,r=""+new URL("Group263.9e7a1c61.svg",import.meta.url).href;export{e as _,r as a};
--- dist/_nuxt/Settings.10dd2299.mjs
+++
@@ -1,1 +0,0 @@
-const t=""+new URL("Settings.7cd52b91.svg",import.meta.url).href;export{t as _};
